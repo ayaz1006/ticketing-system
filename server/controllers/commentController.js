e84b9b0297df824b53297f538e1f5f55c8d328ba
Ayaz Ali <ayazali.9910@gmail.com>
2026-08-23 23:31:35 +0530

    adds comments APIs

---
+++ server/controllers/commentController.js
@@ -0,0 +1,58 @@
+import prisma from "../prisma/client.js";
+
+// Add Comment
+export const addComment = async (req, res) => {
+  try {
+    const { userId } = await req.auth();
+    const { content, taskId } = req.body;
+
+    // check if user is project member
+
+    const task = await prisma.task.findUnique({
+      where: { id: taskId },
+    });
+
+    const project = await prisma.project.findUnique({
+      where: { id: task.projectId },
+      include: { member: { include: { user: true } } },
+    });
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    const member = project.members.find((member) => member.userId === userId);
+    if (!member) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to comment on this task" });
+    }
+
+    const comment = await prisma.comment.create({
+      data: { taskId, content, userId },
+      include: { user: true },
+    });
+    res.status(201).json({ comment });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message || error.code || "Internal Server Error",
+    });
+  }
+};
+
+// Get Comments for task
+export const getTaskComments = async (req, res) => {
+  try {
+    const { taskId } = req.params;
+    const comments = await prisma.comment.findmany({
+      where: { taskId },
+      include: { user: true },
+    });
+    res.json({ comments });
+  } catch (error) {
+    res.status(500).json({
+      message:
+        error.message || error.code || error.message || "Internal Server Error",
+    });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { inngest, functions } from "./inngest/index.js";
 import workspaceRouter from "./routes/workspaceRoutes.js";
 import projectRouter from "./routes/projectRoutes.js";
 import taskRouter from "./routes/taskRoutes.js";
+import commentRouter from "./routes/commentRoutes.js";
 import { protect } from "./middlewares/middleware.js";
 
 const app = express();
@@ -23,6 +24,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/workspaces", protect, workspaceRouter);
 app.use("/api/projects", protect, projectRouter);
 app.use("/api/tasks", protect, taskRouter);
+app.use("/api/comments", protect, commentRouter);
 
 const PORT = process.env.PORT || 5001;
 
